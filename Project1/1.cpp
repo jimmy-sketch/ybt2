413bdb6bb1c50467bea57f2dc51accc66a956ca6
ji'm'my <ji'm'my@DESKTOP-C7OGU26>
2022-10-30 15:20:42 +0800

    添加项目文件。

---
+++ Project1/1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	double sum = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		double x, y;
+		int people;
+		cin >> x >> y >> people;
+		double dist = sqrt(x * x + y * y);
+		dist = dist * 2;
+		double thistime = people * 1.0 + dist / 50 * 1.0 + people * 0.5;
+		sum += thistime;
+	}
+	
+}
